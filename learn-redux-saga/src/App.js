fe41154a4164f6f55112dbe9b557bfc9796f9b73
jsho45n <tpgns20233@gmail.com>
2019-12-23 18:23:18 +0900

    redux-saga-study

---
+++ learn-redux-saga/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SampleContainer from './containers/Samplecontainer';
+
+
+function App() {
+  return (
+    <div>
+      <SampleContainer />
+    </div>
+  );
+}
+
+export default App;
